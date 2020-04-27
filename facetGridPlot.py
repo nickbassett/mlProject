664a4f6dd53a4b059964c07edee56994534e4122
Nick <nickzbass@gmail.com>
2020-04-27 22:22:27 +0100

    Added facetGridPlot.py

---
+++ facetGridPlot.py
@@ -0,0 +1,14 @@
+# Import the required libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the Iris dataset
+iris = sns.load_dataset("iris")
+
+# Generate the Facet Grid plot
+sns.FacetGrid(iris,hue="species",size=6) \
+.map(plt.scatter,"sepal_length","sepal_width") \
+.add_legend()
+
+# Display the plot
+plt.show()
