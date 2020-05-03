05081c975916c180377b1079ef8f2e8869ec0a19
nickbassett <nickzbass@gmail.com>
2020-05-03 21:41:41 +0100

    Modified to limit the tree depth to 3 and the ASM was changed to entropy

--- diabetesDT.py
+++ diabetesDT.py
@@ -22,7 +22,8 @@ y = pima.label # Target variable
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) 
 
 # Create Decision Tree classifer object
-clf = DecisionTreeClassifier()
+#clf = DecisionTreeClassifier()
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=3) #limit tree depth to 3 and ASM to entropy
 
 # Train Decision Tree Classifer
 clf = clf.fit(X_train,y_train)
