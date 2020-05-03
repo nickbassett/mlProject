75df8161cb7ea0435543bc015955ff7c3f649c01
nickbassett <nickzbass@gmail.com>
2020-05-03 21:22:00 +0100

    Updated diabetesDT.py

--- diabetesDT.py
+++ diabetesDT.py
@@ -1,13 +1,16 @@
-# Load libraries
+# Load all required libraries
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
-from sklearn.model_selection import train_test_split # Import train_test_split function
-from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO
+from sklearn import tree
+import pydotplus
+import collections
 col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
 
 # Load dataset
 pima = pd.read_csv("diabetes.csv", header=None, names=col_names)
-print(pima.head())
 
 #split dataset in features and target variable
 feature_cols = ['pregnant', 'insulin', 'bmi', 'age', 'glucose', 'bp', 'pedigree']
@@ -16,16 +19,22 @@ y = pima.label # Target variable
 
 # Split dataset into training set and test set
 # 70% training and 30% test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) 
 
 # Create Decision Tree classifer object
 clf = DecisionTreeClassifier()
 
 # Train Decision Tree Classifer
 clf = clf.fit(X_train,y_train)
-
-#Predict the response for test dataset
-y_pred = clf.predict(X_test.drop(0))
-
-# Model accuracy
-print("Accuracy:",metrics.accuracy_score(y_test.drop(0), y_pred))
+dot_data = tree.export_graphviz(clf, out_file=None, filled=True, rounded=True, special_characters=True, feature_names= feature_cols, class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data)
+colors = ('turquoise', 'orange')
+edges = collections.defaultdict(list)
+for edge in graph.get_edge_list():
+    edges[edge.get_source()].append(int(edge.get_destination()))
+for edge in edges:
+    edges[edge].sort()
+    for i in range(2):
+        dest = graph.get_node(str(edges[edge][i]))[0]
+        dest.set_fillcolor(colors[i])
+graph.write_png('tree.png')
